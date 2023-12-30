46170069c8e05e1be226b0a5828525c55193b75a
Andy Shiue <andy.shiue@moiggi.com>
2023-12-29 22:07:06 -0500

    Fixed the shared_ptr support in VertexShader and FragmentShader

--- include/Vulk/FragmentShader.h
+++ include/Vulk/FragmentShader.h
@@ -17,6 +17,24 @@ class FragmentShader : public ShaderModule {
       const std::string& type,
       uint32_t binding,
       VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
+
+  //
+  // Override the sharable types and functions
+  //
+  using shared_ptr = std::shared_ptr<FragmentShader>;
+  using weak_ptr   = std::weak_ptr<FragmentShader>;
+
+  template <class... Args>
+  static shared_ptr make_shared(Args&&... args) {
+    return std::make_shared<FragmentShader>(std::forward<Args>(args)...);
+  }
+
+  shared_ptr get_shared() {
+    return std::static_pointer_cast<FragmentShader>(ShaderModule::get_shared());
+  }
+  weak_ptr get_weak() {
+    return std::static_pointer_cast<FragmentShader>(ShaderModule::get_weak().lock());
+  }
 };
 
 NAMESPACE_END(Vulk)
--- include/Vulk/VertexShader.h
+++ include/Vulk/VertexShader.h
@@ -41,6 +41,24 @@ class VertexShader : public ShaderModule {
   const std::vector<VertexInputAttribute>& vertexInputAttributes() const {
     return _vertexInputAttributes;
   }
+
+  //
+  // Override the sharable types and functions
+  //
+  using shared_ptr = std::shared_ptr<VertexShader>;
+  using weak_ptr   = std::weak_ptr<VertexShader>;
+
+  template <class... Args>
+  static shared_ptr make_shared(Args&&... args) {
+    return std::make_shared<VertexShader>(std::forward<Args>(args)...);
+  }
+
+  shared_ptr get_shared() {
+    return std::static_pointer_cast<VertexShader>(ShaderModule::get_shared());
+  }
+  weak_ptr get_weak() {
+    return std::static_pointer_cast<VertexShader>(ShaderModule::get_weak().lock());
+  }
 };
 
 NAMESPACE_END(Vulk)
